6fd5692024df41b7f3a03b38f1e193c71d3d2aee
Rico00121 <837043207@qq.com>
2023-07-05 13:53:50 +0800

    feat: calculate winner

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick(i) {
+        const winner = calculateWinner(squares);
+        if (winner) {
+            window.alert("winner is: " + winner);
+            return;
+        }
         if (squares[i]) {
             return;
         }
@@ -12,6 +17,8 @@ export default function Board() {
         xIsNext ? nextSquares[i] = "X" : nextSquares[i] = "O";
         setSquares(nextSquares);
         setXIsNext(!xIsNext);
+
+
     }
     return (
         <>
@@ -32,6 +39,26 @@ export default function Board() {
             </div>
         </>
     );
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 function Square({value, onSquareClick}) {
